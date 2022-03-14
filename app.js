5cec58fdd2c5973b31beeff8458a59942de8884c
Durjoy <da-durjoy@outlook.com>
2022-03-14 14:08:54 +0600

    Routine Published

--- app.js
+++ app.js
@@ -1,15 +1,50 @@
 $('#cs313').on('click', ()=>{
     swal({
             icon:"info",
-            title:"This class conducted in offline",
-            text:"Thank you",
+            title:"Data Communication class conducted in offline",
+            text:"ROOM NO-517",
             button:"close"          
-    });
-    
+    });    
 });
 $('#cs317').on('click', ()=>{
-swal("This class conduted in online","Google Classroom Code: XYZ5Q")
-    .then(() => {
-  swal("Thank You","","success");
-});    
+  swal({
+          icon:"info",
+          title:"Computer Networks class conducted in offline",
+          text:"ROOM NO-507",
+          button:"close"          
+  });    
 });
+$('#cs318').on('click', ()=>{
+  swal({
+          icon:"info",
+          title:"Computer Networks Laboratory class conducted in offline",
+          text:"ROOM NO-220",
+          button:"close"          
+  });    
+});
+
+$('#cs319').on('click', ()=>{
+  swal({
+          icon:"info",
+          title:"Theory of computing class conducted in offline",
+          text:"ROOM NO-517",
+          button:"close"          
+  });    
+});
+
+$('#cs413').on('click', ()=>{
+  swal({
+          icon:"info",
+          title:"Computer Architecture class conducted in offline",
+          text:"ROOM NO-516",
+          button:"close"          
+  });    
+});
+
+
+// $('#cs317').on('click', ()=>{
+// swal("This class conduted in online","Google Classroom Code: XYZ5Q")
+//     .then(() => {
+//   swal("Thank You","","success");
+// });    
+// });
